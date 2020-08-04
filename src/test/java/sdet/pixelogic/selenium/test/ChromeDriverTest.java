1714d9dc7980855e642ba5ab2e4e9e030229cf6d
Samir Mayada <mayada.samir@orange.com>
2020-08-04 04:57:31 +0200

    first commit from git

---
+++ src/test/java/sdet/pixelogic/selenium/test/ChromeDriverTest.java
@@ -0,0 +1,175 @@
+package sdet.pixelogic.selenium.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class ChromeDriverTest {
+
+	ChromeDriver driver;
+	Faker fakeData = new Faker();
+	String firstname = fakeData.name().firstName();
+	String lastname = fakeData.name().lastName();
+	String phone = fakeData.phoneNumber().cellPhone();
+	String email = fakeData.internet().emailAddress();
+	String password = fakeData.lorem().characters(8, true);
+
+	@BeforeTest
+	public void openURL()
+	{
+		//Add Chrome Driver executable file in the system properties
+		String ChromePath = System.getProperty("user.dir")+"\\resources\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver",ChromePath);
+
+		//Take new object from Chrome Driver
+		driver = new ChromeDriver();
+
+		//Open Google URL from WebDriver
+		driver.navigate().to("https://www.phptravels.net/register");
+
+	}
+
+	@Test(priority = 0)
+	@Description("Sign Up Test Case")
+	public void SignUp() 
+	{
+		WebElement firstnameTxt    = driver.findElement(By.name("firstname"));
+		WebElement lastnameTxt     = driver.findElement(By.name("lastname"));
+		WebElement phoneTxt        = driver.findElement(By.name("phone"));
+		WebElement emailTxt        = driver.findElement(By.name("email"));
+		WebElement passwordTxt     = driver.findElement(By.name("password"));
+		WebElement cfmpasswordTxt  = driver.findElement(By.name("confirmpassword"));
+
+		firstnameTxt.clear();
+		firstnameTxt.sendKeys(firstname);
+
+		lastnameTxt.clear();
+		lastnameTxt.sendKeys(lastname);
+
+		phoneTxt.clear();
+		phoneTxt.sendKeys(phone);
+
+		emailTxt.clear();
+		emailTxt.sendKeys(email);
+
+		passwordTxt.clear();
+		passwordTxt.sendKeys(password);
+
+		cfmpasswordTxt.clear();
+		cfmpasswordTxt.sendKeys(password);
+
+		//Hitting enter to Sign Up
+		//cfmpasswordTxt.sendKeys(Keys.RETURN);
+		cfmpasswordTxt.submit();
+		
+		//Add wait time to handle low speed internet connection
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleContains("My Account"));
+		
+		//Validate that it has logged in successfully
+		String successLogin = driver.getCurrentUrl();
+		Assert.assertEquals(successLogin,"https://www.phptravels.net/account/");
+	}
+	
+	@Test(priority = 1)
+	@Description("Logout Test Case")
+	@Attachment
+	public void Logout()
+	{
+		WebElement account        = driver.findElement(By.cssSelector(".bx.bx-user"));
+		account.click();
+		
+		WebElement logout        = driver.findElement(By.xpath("//*[@id=\"//header-waypoint-sticky\"]/div[1]/div/div/div[2]/div/ul/li[3]/div/div/div/a[2]"));
+		logout.click();
+		
+		//Validate that it has logged in successfully
+		String successLogout = driver.getCurrentUrl();
+		Assert.assertEquals(successLogout,"https://www.phptravels.net/login");
+	}
+	
+	@Test(priority = 2)
+	@Description("Login Test Case")
+	@Attachment(value = "Screenshot", type = "image/png")
+	public void Login()
+	{
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleContains("Login"));
+		
+		WebElement emailTxt        = driver.findElement(By.name("usernamee"));
+		WebElement passwordTxt     = driver.findElement(By.name("password"));
+		
+		emailTxt.clear();
+		emailTxt.sendKeys(email);
+
+		passwordTxt.clear();
+		passwordTxt.sendKeys(password);
+		
+		//Hitting enter to Login
+		passwordTxt.sendKeys(Keys.RETURN);
+		
+		//Add wait time to handle low speed internet connection
+		wait.until(ExpectedConditions.titleContains("My Account"));
+		
+		//Validate that it has logged in successfully
+		String successLogin = driver.getCurrentUrl();
+		Assert.assertEquals(successLogin,"https://www.phptravels.net/account/");
+		
+		try 
+		{
+			// Injecting a failure here to show case a failure
+			Assert.assertEquals(driver.findElement(By.id("firstHeading")).getText(), "HP ALM");		
+		} catch (AssertionError err) {
+			takeScreenshot();
+			throw err;
+		}
+	}
+	
+	@AfterMethod
+	public void SignUpFailScreenshot(ITestResult result) throws IOException
+	{
+		if(ITestResult.FAILURE == result.getStatus())
+		{
+			//Create reference of TakeScreenshots
+			TakesScreenshot ts = (TakesScreenshot)driver;
+			File source = ts.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(source, new File("./ScreenshotsOfFailures/"+ result.getName() + ".png"));
+			driver.quit();
+		}
+	}
+	
+	@AfterTest
+	public void closeDriver() 
+	{
+		driver.quit();
+	}
+	
+	@Attachment(value = "Screenshot", type = "image/png")
+	public byte[] takeScreenshot() 
+	{
+			// Take a screenshot as byte array and return
+			return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+	}
+}
